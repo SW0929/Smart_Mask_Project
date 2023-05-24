5bed31f5141efa2e59cb2efa742a49310a23f2b4
sw <sjugas1234@naver.com>
2023-05-24 16:57:41 +0900

    05/24/23_01

--- app/src/main/java/com/bliss/csc/smart_mask_sw9909/fragment/InfoFragment.kt
+++ app/src/main/java/com/bliss/csc/smart_mask_sw9909/fragment/InfoFragment.kt
@@ -1,9 +1,14 @@
 package com.bliss.csc.smart_mask_sw9909.fragment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bliss.csc.smart_mask_sw9909.databinding.InfoFragmentBinding
 import com.bliss.csc.smart_mask_sw9909.fragment.chat.ChatBot
@@ -24,26 +29,63 @@ class InfoFragment: Fragment() {
 
         // recyclerView 사용
 
-        val apiKey = "*************************************" // todo 개인 gpt 키 사용
+        val apiKey = "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" // todo 개인 gpt 키 사용
         val baseUrl = "https://api.openai.com/v1/chat/completions"
         val chatGptModel = "gpt-3.5-turbo" // 또는 원하는 ChatGPT 모델 지정
 
         chatBot = ChatBot(apiKey, baseUrl, chatGptModel)
 
         binding.sendButton.setOnClickListener {
+
             val message = binding.userInputEditText.text.toString().trim()
 
+            //User 가 질문을 입력하면 chatBot에서 답을 출력
             if (message.isNotEmpty()) {
                 binding.userInputEditText.text.clear()
+                //왜 키보드가 안내려갈까
                 chatBot.sendMessage(message) { reply ->
                     activity?.runOnUiThread {
                         reply?.let {
-                            binding.chatTextView.append("User: $message\n")
-                            binding.chatTextView.append("ChatBot: $it\n\n")
+                            val userText = "User: $message\n"
+                            val chatBotText = "ChatBot: $it\n\n"
+
+                            val spannableString = SpannableString(userText + chatBotText)
+
+                            // User 텍스트에 다른 색상 적용
+                            spannableString.setSpan(
+                                ForegroundColorSpan(Color.BLACK),
+                                0, // 시작 인덱스
+                                userText.length, // 끝 인덱스
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+
+                            // ChatBot 텍스트에 다른 색상 적용
+                            spannableString.setSpan(
+                                ForegroundColorSpan(Color.GREEN),
+                                userText.length, // 시작 인덱스
+                                spannableString.length, // 끝 인덱스
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+
+                            binding.chatTextView.append(spannableString)
+
                         }
                     }
                 }
             }
+
+//            if (message.isNotEmpty()) {
+//                binding.userInputEditText.text.clear()
+//                chatBot.sendMessage(message) { reply ->
+//                    activity?.runOnUiThread {
+//                        reply?.let {
+//
+//                            binding.chatTextView.append("User: $message\n")
+//                            binding.chatTextView.append("ChatBot: $it\n\n")
+//                        }
+//                    }
+//                }
+//            }
         }
 
         return binding.root
